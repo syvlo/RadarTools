52c69aeb2451bae6bd313db714d612ea09e96f48
Sylvain Lobry <sylvain.lobry@telecom-paristech.fr>
2014-06-26 11:01:08 +0200

    Added some options to imw2png.
    Includes the possibility to set a range instead of a sigma for [0; mu+n sigma].
    
    		 * src/imw2png/main.cc: Here.

--- src/imw2png/main.cc
+++ src/imw2png/main.cc
@@ -2,41 +2,78 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cstring>
 #include <cv.h>
 #include <highgui.h>
 
 void
 printHelp()
 {
-	std::cout << "imw2png input[.imw] n [output]" << std::endl << std::endl
+	std::cout << "imw2png input[.imw] [-o output] [-NSigma n] [-upper up]"
+			  << std::endl << std::endl
 			  << "Will save input.imw as output.png (or input.png if output"
-			  << std::endl << "is not set) with values map from " << std::endl
-			  << "[0; mu + n * sigma] to [0; 255] with n a float" << std::endl;
+			  << std::endl << "is not set) with values map from:" << std::endl
+			  << " - [0; up] to [0; 255] if up is set;" << std::endl
+			  << " - [0; mu + n * sigma] to [0; 255] if n is set" << std::endl
+			  << " - [0; mu + 3 * sigma] to [0; 255] otherwise." << std::endl;
 }
 
 
 int main (int argc, char* argv[])
 {
-	if (argc != 3 && argc != 4)
+	if (argc < 2)
 	{
 		printHelp();
 		return 1;
 	}
 
-	std::string inputFileName(argv[1]);
+	double n = -1;
+	std::string outputFileName = "";
+	double upperVal = -1;
+
+	std::string inputFileName;
+	for (int i = 1; i < argc; ++i)
+	{
+		if (!strcmp("-NSigma", argv[i]))
+		{
+			n = atof(argv[++i]);
+		}
+		else if (!strcmp("-o", argv[i]))
+		{
+			outputFileName = argv[++i];
+		}
+		else if (!strcmp("-upper", argv[i]))
+		{
+			upperVal = atof(argv[++i]);
+		}
+		else if (!strcmp("-h", argv[i]) || !strcmp("--help", argv[i]))
+		{
+			printHelp();
+			return 0;
+		}
+		else
+			inputFileName = argv[i];
+	}
 
 	if (inputFileName.length() > 4 && inputFileName.substr(inputFileName.length() - 4).compare(".imw") == 0)
 		inputFileName.erase(inputFileName.length() - 4);
 
 	cv::Mat SARImage = ReadImw(inputFileName.c_str());
-	cv::Scalar mean;
-	cv::Scalar stddev;
 
-	cv::meanStdDev(SARImage, mean, stddev);
+	if (upperVal == -1)
+	{
+		cv::Scalar mean;
+		cv::Scalar stddev;
 
-	double n = atof(argv[2]);
+		cv::meanStdDev(SARImage, mean, stddev);
 
-	double upperVal = mean.val[0] + n * stddev.val[0];
+		if (n == -1)
+			n = 3;
+
+		upperVal = mean.val[0] + n * stddev.val[0];
+	}
+
+	std::clog << "Mapping [0; " << upperVal << "] to [0; 255]" << std::endl;
 
 	cv::Mat Output(SARImage.size(), CV_8U);
 
@@ -52,13 +89,13 @@ int main (int argc, char* argv[])
 		}
 	}
 
-	std::string outputFileName;
-	if (argc == 4)
-		outputFileName = argv[3];
-	else
-		outputFileName = inputFileName;
 
-	outputFileName += ".png";
+	if (outputFileName.compare("") == 0)
+	{
+		outputFileName = inputFileName;
+		outputFileName += ".png";
+	}
+
 
 	std::clog << "Writing to " << outputFileName.c_str() << std::endl;
 	cv::imwrite(outputFileName.c_str(), Output);
